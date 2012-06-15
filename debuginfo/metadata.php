1a2c606412f3d0abe7200b0c31ea76961ccffa54
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2012-06-15 10:00:41 +0300

    Fixed file header

--- debuginfo/metadata.php
+++ debuginfo/metadata.php
@@ -1,14 +1,5 @@
 <?php
 /**
- * #PHPHEADER_OXID_LICENSE_INFORMATION#
- *
- * @link      http://www.oxid-esales.com
- * @package   main
- * @copyright (c) OXID eSales AG 2003-#OXID_VERSION_YEAR#
- * @version   SVN: $Id: $
- */
-
-/**
  * Metadata version
  */
 $sMetadataVersion = '1.0';
